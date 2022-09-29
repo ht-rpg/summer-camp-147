a3e40b62e63a8c34325ebeaf67aa71159da90f8f
ElieneMontenegro <lila.amaral@hotmail.com>
2022-09-29 04:04:27 -0300

    ht enigma

---
+++ main.js
@@ -0,0 +1,9 @@
+function popUp() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function popUp2() {
+  var popup2 = document.getElementById("myPopup2");
+  popup2.classList.toggle("show2");
+}
